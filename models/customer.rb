1f3e3cdf1873067d8cfa317ea0e26fea25f50d20
Emma <emmagray183@gmail.com>
2019-11-22 17:49:26 +0000

    films method for customer created and tested

--- models/customer.rb
+++ models/customer.rb
@@ -42,4 +42,12 @@ class Customer
     SqlRunner.run(sql, values)
   end
 
+  def films
+    sql = "SELECT films.* FROM films INNER JOIN tickets
+    ON films.id = film_id WHERE customer_id = $1"
+    values = [@id]
+    films = SqlRunner.run(sql, values)
+    films.map { |film| Film.new(film)}
+  end
+
 end
