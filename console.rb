3e614792cea23fc3723d868d932636922067a29b
Emma <emmagray183@gmail.com>
2019-11-22 15:45:39 +0000

    all method for customer added and tested

--- console.rb
+++ console.rb
@@ -12,6 +12,10 @@ Ticket.delete_all
 
 customer1 = Customer.new({"name" => "Brendan", "funds" => 100.00})
 customer1.save
+customer2 = Customer.new({"name" => "Debbie", "funds" => 100.00})
+customer2.save
+customer3 = Customer.new({"name" => "Grant", "funds" => 100.00})
+customer3.save
 
 film1 = Film.new({"title" => "Joker", "price" => 10.00})
 film1.save
@@ -21,3 +25,6 @@ screening1.save
 
 ticket1 = Ticket.new({"customer_id" => customer1.id, "film_id" => film1.id, "screening_id" => screening1.id})
 ticket1.save
+
+binding.pry
+nil
